9c52d6dac93189a6d91004fe8214ea7b85b0f8dc
Abdur Rahim <47345730+arfat01850@users.noreply.github.com>
2019-05-10 17:15:12 +0600

    Add files via upload

---
+++ C.subin/4.10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int i, n;
+    for(n = 1; n<=20; n = n+1){
+        for(i = 1; i<= 10; i = i+1){
+            printf("%d X %d = %d\n", n, i, n*i);
+        }
+    }
+    return 0;
+}
